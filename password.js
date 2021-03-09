f1ac32f54ea3fe8df87f73a1e155ce84bcbf8864
David Andersson <lejonmanen@gmail.com>
2021-03-09 13:28:08 +0100

    Level 2

--- password.js
+++ password.js
@@ -1,6 +1,58 @@
 /*
-Skapa en enklare lösenordsgenerator i form av en function passwordGenerator(length) där enda argumentet är längd på lösenordet. Funktionen ska returnera ett slumpat lösenord som visas på skärmen. Använd Math.random() för att skapa slumptal.
+Se till att din lösenordsgenerator genererar lösenord som uppfyller följande krav:
+
+Minst 10 tecken
+En siffra
+En stor bokstav
+En liten bokstav
+Ett specialtecken (!"#$%&'()*+,-./:;<=>?@[\]^{|})
 */
+function passwordGenerator2() {
+	const characters = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&'()*+,-./:;<=>?@[\]^{|}`;
+
+	let password = '';
+	for(let i=0; i<10; i++) {
+		let randomIndex = Math.floor(Math.random() * characters.length);
+		let newCharacter = characters[randomIndex];
+		password += newCharacter;
+	}
+
+	// kontrollera om det är okej
+	if( hasCapitalLetter(password) && hasLowercaseLetter(password) && hasNumber(password) && hasSpecialCharacter(password) ) {
+		return password;
+	}
+
+	console.log('Misslyckades med att skapa ett bra lösenord: ' + password);
+	return passwordGenerator2();  // rekursion
+}
+/* Alternativ metod:
+function hasCapitalLetter(string) {
+	const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	for( let i=0; i<string.length; i++ ) {
+		let c = string.charAt(i);
+		if( capitalLetters.includes(c) ) {
+			return true;
+		}
+	}
+	return false;
+}*/
+function hasCharacter(string, characters) {
+	// Higher order functions: find, filter, map, reduce, some, every
+	let array = string.split('');
+	return array.some(c => characters.includes(c));
+}
+function hasCapitalLetter(string) {
+	return hasCharacter(string, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+}
+function hasLowercaseLetter(string) {
+	return hasCharacter(string, 'abcdefghijklmnopqrstuvwxyz');
+}
+function hasNumber(string) {
+	return hasCharacter(string, '0123456789');
+}
+function hasSpecialCharacter(string) {
+	return hasCharacter(string, `!"#$%&'()*+,-./:;<=>?@[\]^{|}`);
+}
 
 function passwordGenerator(length) {
 	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -16,5 +68,5 @@ function passwordGenerator(length) {
 	return password;
 }
 
-let pw = passwordGenerator(8);
+let pw = passwordGenerator2();
 console.log('Vi skapade lösenordet: ' + pw);
